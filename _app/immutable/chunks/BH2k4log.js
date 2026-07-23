3a1a8b0b6b28263599b4d7844939c3dcc5b3bf11
Alexander Vega <alxvj2@gmail.com>
2026-07-23 02:11:50 +0000

    Deploy

---
+++ _app/immutable/chunks/BH2k4log.js
@@ -0,0 +1,1 @@
+const s=globalThis.__sveltekit_xap9w?.base??"/point-it-out-docs",t=globalThis.__sveltekit_xap9w?.assets??s??"";export{t as a,s as b};
---
+++ _app/immutable/entry/start.CBzdADY7.js
@@ -0,0 +1,1 @@
+import{l as o,c as r}from"../chunks/CbELb7XD.js";export{o as load_css,r as start};
